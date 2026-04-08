e44bc0b4d7cb0e6ef3c90eafa27f593d3efb059b
DEMANGE Martin <alextrav.12354@gmail.com>
2026-04-08 14:44:12 +0200

    feat : added the class Commands.java, this class will be used as the interface of the api.

---
+++ Commandes/src/main/java/fr/univamu/iut/commandes/Commands.java
@@ -0,0 +1,75 @@
+package fr.univamu.iut.commandes;
+
+import jakarta.ws.rs.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Manage the commands, that are stored in the api
+ */
+@Path("/commandes")
+public class Commands {
+
+    // url : http://localhost:8082/commandes/api
+
+    public Commands() {
+    }
+
+    /**
+     * Return all the commands made by a specific subscriber.
+     * @param subscriberId The id of the subscriber that ordered the commands.
+     */
+    @Path("{subscriberId}")
+    @GET
+    public String getAllCommandes(Integer subscriberId){
+        //TODO : implement it, more info in openapi-commandes.yaml.yaml
+        return null; //TODO replace this stub to something useful
+    }
+
+    /**
+     *
+     * @param subscriberId The id of the subscriber that made the command.
+     * @param adrsDelivery The address of delivery.
+     * @param deadline The deadline of the command.
+     * @param menus A map where the key is the id of a menu, and the value the quantity of the key menu.
+     */
+    @POST
+    public void CreateCommande(Integer subscriberId, String adrsDelivery, String deadline, Map<Integer,Integer> menus){
+        // TODO : implement it
+    }
+
+    /**
+     * Get a specific command by using his id.
+     * @param id The id of the command
+     * @return TODO: implement it
+     */
+    @Path("{id}")
+    @GET
+    public String getCommandeById(Integer id){
+        //TODO : implement it
+        return null; //TODO replace this stub to something useful
+    }
+
+    /**
+     * Update the address of delivery and the deadline of a command
+     * @param id The id of the command to update.
+     * @param adrsDelivery The new address of delivery.
+     * @param deadline The new deadline.
+     */
+    @PUT
+    public void updateCommande(Integer id, String adrsDelivery, String deadline){
+        // TODO : implement it
+    }
+
+    /**
+     * Delete the given command.
+     * @param id The id of the command to be deleted.
+     */
+    @DELETE
+    public void deleteCommande(Integer id){
+        //TODO : implement it
+    }
+
+
+}
